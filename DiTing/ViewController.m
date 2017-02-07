e86c073803710c7157055cae42e432b53fcb8baf
ZYYNN <zhuynchn@163.com>
2017-02-07 23:42:32 +0800

    1

--- DiTing/ViewController.m
+++ DiTing/ViewController.m
@@ -14,9 +14,11 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    
 }
 
 
