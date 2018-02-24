ac67bcd3801ef6e6c2fe5029250dc5fb1fc9eb94
ZYYNN <zhuynchn@163.com>
2018-02-24 23:35:27 +0800

    add compoent

--- DITSiriExtension/IntentHandler.m
+++ DITSiriExtension/IntentHandler.m
@@ -27,6 +27,10 @@
     // This is the default implementation.  If you want different objects to handle different intents,
     // you can override this and return the handler you want for that particular intent.
     
+    NSLog(@"%@", intent.description);
+    NSLog(@"%@", intent.identifier);
+    NSLog(@"%@", intent);
+    
     return self;
 }
 
