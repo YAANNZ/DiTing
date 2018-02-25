80dee7628ba764199417eaa26845bcd117c5c52f
ZYYNN <zhuynchn@163.com>
2018-02-25 22:00:45 +0800

    LoopView

---
+++ DiTing/Class/DITCompLoopViewController.h
@@ -0,0 +1,13 @@
+//
+//  DITCompLoopViewController.h
+//  DiTing
+//
+//  Created by ZHUYN on 2018/2/25.
+//  Copyright © 2018年 zyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DITCompLoopViewController : UIViewController
+
+@end
---
+++ DiTing/Class/DITCompLoopViewController.m
@@ -0,0 +1,39 @@
+//
+//  DITCompLoopViewController.m
+//  DiTing
+//
+//  Created by ZHUYN on 2018/2/25.
+//  Copyright © 2018年 zyn. All rights reserved.
+//
+
+#import "DITCompLoopViewController.h"
+
+@interface DITCompLoopViewController ()
+
+@end
+
+@implementation DITCompLoopViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
