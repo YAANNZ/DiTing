395b97f6f571e1abcd6d89071613186dd1245c0a
yanan.zhu <yanan.zhu@renren-inc.com>
2022-11-18 16:29:58 +0800

    feat: add setting

---
+++ DiTing/Class/Setting/DITSettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DITSettingViewController.swift
+//  DiTing
+//
+//  Created by yanan.zhu on 2022/11/18.
+//  Copyright © 2022 zyn. All rights reserved.
+//
+
+import UIKit
+
+class DITSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
